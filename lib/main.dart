c86e9e4d80325dfc53effea73ee7e8e2c7879c09
sajitha00 <sajithageevinda.sd@gmail.com>
2024-03-31 13:19:05 +0530

    added verification page

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,12 @@ import 'package:recipiapp/Screens/forgotPassword.dart';
 import 'package:recipiapp/Screens/home.dart';
 import 'package:recipiapp/Screens/register.dart';
 import 'package:recipiapp/Screens/settings.dart';
+import 'package:recipiapp/Screens/verification.dart';
 import 'package:recipiapp/Screens/welcome.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home:ForgotPassword(),
+    home:Verfication(),
   ));
 }
