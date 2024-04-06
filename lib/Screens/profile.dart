5ff5b41c640dfe75b06b109bb1a79b4094a25e5b
Sajitha Geevinda Kodithuwakku <115799454+sajitha00@users.noreply.github.com>
2024-04-06 23:10:36 +0530

    added completed profile page

--- lib/Screens/profile.dart
+++ lib/Screens/profile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:recipiapp/components/bottomNavigation.dart';
@@ -14,6 +16,10 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
+    String name = "Sunmanasiri Sarabawan";
+    String subText = "Sunmanasiri";
+    String description = "Flaky flatbreads are served alongside curries or stuffed with flavorful fillings.";
+    String imgPath = "images/containImg.png";
     return ScaffoldGradientBackground(gradient:LinearGradient(
       colors: [
         Color(0xFF444141), // #444141
@@ -45,45 +51,152 @@ class _ProfileState extends State<Profile> {
                 Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(25),
-                    color: Colors.white
+                    color: Color(0xFFD0D0D0),
                   ),
                   child:  Padding(
                     padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    child: Column(
+                     // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Row(
                           children: [
-                            InkWell(
-                                onTap: (){},
-                                child: Image.asset("images/Profileuser.png")),
+                            Image.asset("images/Profileuser.png"),
                             SizedBox(width: 10,),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("Ganidu Happuarachhi",style: TextStyle(
-                                    fontFamily: "Poppins",
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 15,
-                                    color: Colors.black
-                                ),),
-                                Text("Restaurant Chef:",style: TextStyle(
-                                    fontFamily: "Poppins",
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 15,
-                                    color: Colors.black
-                                ),),
-                                Row(
+                            Expanded(
+                                child:Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Text("data")
+                                    Text("Ganindu Happuarachchi",style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontFamily:"Poppins",
+                                      fontSize: 15,
+                                    ),),
+                                    SizedBox(height: 4,),
+                                    Text("Restaurant Chef:",style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontFamily:"Poppins",
+                                      fontSize: 14,
+                                    ),),
+                                    SizedBox(height: 5,),
+                                    Text("The backbone of the professional kitchen, leading a team and ensuring smooth operations while creating delicious dishes. They may specialize in a certain cuisine or be known for their innovative flair.")
                                   ],
-                                )
-                              ],
+
+                            ),),
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          children: [
+                            Expanded(
+                                child: ElevatedButton(
+                                  onPressed: (){},
+                                     child: Text("Follow",style: TextStyle(
+                                       fontSize: 20,
+                                       fontWeight: FontWeight.w600,
+                                       fontFamily: "Poppins",
+                                       color: Colors.white,
+                                     ),),
+                                  style: ButtonStyle(
+                                      backgroundColor: MaterialStatePropertyAll(
+                                        Color.fromRGBO(0, 135, 255, 0.7),s
+                                      ),
+                                  ),
+                                  ),
                             ),
                           ],
                         ),
                       ],
                     ),
+                  ),
+                ),
+                SizedBox(height: 15,),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFFD0D0D0),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Row(
+                          children: [
+                            InkWell(
+                              onTap: (){
+                                print("profile clicked");
+                              },
+                              child: ImageIcon(
+                                AssetImage("images/user2.png"),
+                                size: 35,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(width: 3,),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(name,style:GoogleFonts.inter(
+                                  textStyle: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontWeight:FontWeight.w500,
+
+                                  ),
+                                ) ,
+                                ),
+                                Text(subText,style:GoogleFonts.inter(
+                                  textStyle: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 11,
+                                    fontWeight:FontWeight.w200,
+                                    height: 0.8,
+                                  ),
+                                ) ,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 0.2,),
+                      Image(image: AssetImage(imgPath)
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 6,
+                          right: 6,
+                          top: 4,
+                        ),
+                        child: Text(description,
+                          style:GoogleFonts.inter(
+                            textStyle: TextStyle(
+                                color: Colors.black,
+                                fontSize: 11,
+                                fontWeight:FontWeight.w300
+                            ),
+                          ),
+                        ),
+                      ),
+                      Divider(
+                        color: Colors.black,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 6,
+                          top: 2,
+                          bottom: 6,
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(Icons.favorite_border),
+                            SizedBox(width: 5,),
+                            Icon(Icons.share_rounded),
+                            SizedBox(width: 5,),
+                            Icon(Icons.chat_bubble_outline),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 ),
               ],
